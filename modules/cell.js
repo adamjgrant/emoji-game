775e0b52088befe4b12b7b65e8e31d2702561bde
AG <a@b.com>
2024-03-22 16:24:15 -0700

    Done with cell validation for now

--- modules/cell.js
+++ modules/cell.js
@@ -10,5 +10,17 @@ export class Cell {
     if (this.properties.type === 'HasValue' && this.properties.value === '' && this.properties.answer === '') {
       throw new Error(`Error at cell ${this.row}, ${this.column}: A cell without a value must have an answer`);
     }
+
+    if (this.properties.type === 'HasValue' && this.properties.value === '' && !this.properties.arithmetic) {
+      throw new Error(`Error at cell ${this.row}, ${this.column}: A cell without a value must have an arithmetic operation`);
+    }
+
+    if (this.properties.type === 'HasValue' && this.properties.value !== '' && this.properties.answer !== '') {
+      throw new Error(`Error at cell ${this.row}, ${this.column}: A cell with a value cannot have an answer`);
+    }
+
+    if (this.properties.type === null && this.properties.value !== undefined) {
+      throw new Error(`Error at cell ${this.row}, ${this.column}: A cell with a value cannot have an arithmetic operation`);
+    }
   }
 }
--- test/test.js
+++ test/test.js
@@ -21,37 +21,13 @@ let Game = {
 let cell00 = {
   "type": "HasValue",
   "value": "☁️",
-  "arithmetic": {
-    "operation": "add",
-    "operands": [
-      {
-        "row": 0,
-        "cell": 0
-      },
-      {
-        "row": 0,
-        "cell": 1
-      }
-    ]
-  }
 } 
+
 let cell01 = {
   "type": "HasValue",
   "value": "️💧",
-  "arithmetic": {
-    "operation": "add",
-    "operands": [
-      {
-        "row": 0,
-        "cell": 0
-      },
-      {
-        "row": 0,
-        "cell": 1
-      }
-    ]
-  }
 } 
+
 let cell02 = {
   "type": "HasValue",
   "value": "",
@@ -76,14 +52,28 @@ Game.rows[0].cells.push(cell02);
 
 describe('Cell', function () {
   describe('validation', function () {
-    it('should allow cells with HasValue, an empty string, and an answer', function () {
+    it('should allow cells with HasValue, an empty value, and an answer', function () {
       assert.doesNotThrow(() => new Cell(0, 0, cell02));
     });
 
-    it('should not allow cells with HasValue, an empty string, but no answer', function () {
+    it('should not allow cells with HasValue, an empty value, but no answer', function () {
       let cell02_bad = cell02;
       cell02_bad.answer = "";
       assert.throws(() => new Cell(0, 0, cell02_bad), Error);
+    });
+
+    it('should not allow cells with HasValue, an empty value, but no arithmetic', function () {
+      let cell01_bad = cell01;
+      cell01_bad.value = "";
+      cell01_bad.answer = "☁️";
+      assert.throws(() => new Cell(0, 0, cell01_bad), Error);
+    });
+
+    it('should not allow cells with null, but a value', function () {
+      let cell01_bad = cell01;
+      cell01_bad.type = null;
+      cell01_bad.value = "☁️";
+      assert.throws(() => new Cell(0, 0, cell01_bad), Error);
     });
   });
 });
